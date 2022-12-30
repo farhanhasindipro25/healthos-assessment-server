63e36db38af448683592f6c1660cb7ca955872fb
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-12-30 16:26:57 +0600

    APIs added for reading data of all product categories and all products

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Locating the JSON files and storing them in variables
+const categories = require("./data/categories.json");
+const products = require("./data/products.json");
+
+// API for reading data of all product categories
+app.get("/product-categories", (req, res) => {
+  res.send(categories);
+});
+
+// API for reading data of all products
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
 // API for running the server
 app.get("/", (req, res) => {
-  res.send("Server running.");
+  res.send("String Symphony Server running.");
 });
 
 app.listen(port, () => {
